93350778b1fb1d16af4cebc084b6c8e94e6fa40a
Jorge Luis <jluis.developer@gmail.com>
2022-08-27 10:46:23 -0500

    configure QR in terminal

--- src/infrastructure/repositories/ws.external.ts
+++ src/infrastructure/repositories/ws.external.ts
@@ -64,6 +64,8 @@ class WsTransporter extends Client implements LeadExternal {
   private generateImage = (base64: string) => {
     const path = `${process.cwd()}/tmp`;
     let qr_svg = imageQr(base64, { type: "svg", margin: 4 });
+    var qrcode = require('qrcode-terminal');
+    qrcode.generate(base64, {small: true});
     qr_svg.pipe(require("fs").createWriteStream(`${path}/qr.svg`));
     console.log(`⚡ Recuerda que el QR se actualiza cada minuto ⚡'`);
     console.log(`⚡ Actualiza F5 el navegador para mantener el mejor QR⚡`);
